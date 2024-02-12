3b5f61e17bf24c7e7136fc274f63a0d6682a3072
Your Name <you@example.com>
2024-02-12 22:49:57 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿
+
+using ConsoleApp3;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+[MemoryDiagnoser]
+[RankColumn]
+public class Program
+{
+    
+
+   
+    
+    public static void Main()
+    {
+        BenchmarkRunner.Run<Person>();
+        string s = "abc";
+        Person person = new Person();
+        person.Name = "BOB";
+        person.Foo();
+    }
+    [Benchmark]
+    public void Foo()
+    {
+        string s = "abc";
+        Console.WriteLine(ConsoleApp3.Program.Print(s));
+        
+    }
+
+
+    
+}
+public class Person
+{
+
+    public string Name { get; set; }
+    [Benchmark]
+    public void Foo()
+    {
+        Console.WriteLine(Name);
+
+    }
+}
